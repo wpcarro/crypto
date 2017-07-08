c379cfc7c7f3a9ee4500abc698372c1b73014340
William Carroll <wpcarro@gmail.com>
2017-07-08 18:50:43 -0400

    feat(exchange:xbtce): Supports xBTCe exchange

---
+++ lib/crypto/exchange/xbtce.ex
@@ -0,0 +1,40 @@
+defmodule Crypto.Exchange.XBTCE do
+  @moduledoc """
+  Behaviour module implementing the `Crypto.Exchange` callbacks for the xBTCe exchange.
+
+  """
+
+  alias Crypto.Exchange.XBTCE.HTTP
+
+  @behaviour Crypto.Exchange
+
+
+
+  ################################################################################
+  # Callback Definitions
+  ################################################################################
+
+  def fetch_order_book(asset_pair) do
+    asset_pair =
+      to_asset_pair(asset_pair)
+
+    HTTP.public_get("level2/#{asset_pair}")
+  end
+
+
+  def transaction_fee(_),
+    do: raise("Not implemented.")
+
+
+  def execute_orders(_order),
+    do: raise("Not implemented.")
+
+
+
+  ################################################################################
+  # Private Helpers
+  ################################################################################
+
+  defp to_asset_pair(:eth_usd), do: "eth%20usd"
+  defp to_asset_pair(:ltc_usd), do: "ltc%20usd"
+end
---
+++ lib/crypto/exchange/xbtce/http.ex
@@ -0,0 +1,42 @@
+defmodule Crypto.Exchange.XBTCE.HTTP do
+  @moduledoc """
+  HTTP Driver for xBTCe communication. This module bakes the base URL into the requests. It also
+  signs the requests per the xBTCe specifications.
+
+  ## Rate Limiting
+
+  Public API: xBTCe limits 10 requests a second
+
+  429s are sent to signal the client to backoff.
+
+  """
+
+
+
+  ################################################################################
+  # Constants
+  ################################################################################
+
+  @api_version "v1"
+  @base_url "https://cryptottlivewebapi.xbtce.net:8443/api/#{@api_version}"
+
+
+
+  ################################################################################
+  # Public API
+  ################################################################################
+
+  @doc """
+  Fetches data from xBTCe's public API. Forwards `headers` and `opts` to the `HTTPoison.get!/3`
+  function.
+
+  """
+  @spec public_get(Path.t, keyword, keyword) :: HTTPoison.Response.t
+  def public_get(endpoint, headers \\ [], opts \\ []) do
+    url =
+      Path.join([@base_url, "public", endpoint])
+
+    HTTPoison.get!(url, headers, opts)
+  end
+
+end

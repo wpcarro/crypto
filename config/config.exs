6ce3242df5d2d64defbb2022be1db9daf9b4033f
William Carroll <wpcarro@gmail.com>
2017-07-09 17:02:40 -0400

    feat(config): Checks for sandbox configs at startup

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,31 @@
 use Mix.Config
 
-config :crypto,
-  kraken_api_key: File.read!("config/secrets/kraken_api_key.txt") |> String.trim,
-  kraken_api_secret: File.read!("config/secrets/kraken_api_secret.txt") |> String.trim,
-  coinbase_api_key: File.read!("config/secrets/coinbase_api_key.txt") |> String.trim,
-  coinbase_api_secret: File.read!("config/secrets/coinbase_api_secret.txt") |> String.trim
+
+case true do
+  true ->
+    config :crypto,
+      # Kraken
+      kraken_api_key: File.read!("config/secrets/kraken_api_key.txt") |> String.trim,
+      kraken_api_secret: File.read!("config/secrets/kraken_api_secret.txt") |> String.trim,
+      # GDAX
+      gdax_url: "https://api.gdax.com",
+      gdax_api_key: File.read!("config/secrets/gdax_api_key.txt") |> String.trim,
+      gdax_api_secret: File.read!("config/secrets/gdax_api_secret.txt") |> String.trim,
+      gdax_api_password: File.read!("config/secrets/gdax_api_password.txt") |> String.trim,
+      # Coinbase
+      coinbase_btc_wallet: File.read!("config/secrets/coinbase_btc_wallet_address.txt") |> String.trim,
+      coinbase_eth_wallet: File.read!("config/secrets/coinbase_eth_wallet_address.txt") |> String.trim,
+      coinbase_ltc_wallet: File.read!("config/secrets/coinbase_ltc_wallet_address.txt") |> String.trim,
+      coinbase_usd_wallet: File.read!("config/secrets/coinbase_usd_wallet_address.txt") |> String.trim
+
+  false ->
+    config :crypto,
+      # Kraken
+      kraken_api_key: File.read!("config/secrets/kraken_api_key.txt") |> String.trim,
+      kraken_api_secret: File.read!("config/secrets/kraken_api_secret.txt") |> String.trim,
+      # GDAX
+      gdax_url: "https://api-public.sandbox.gdax.com",
+      gdax_api_key: File.read!("config/secrets/gdax_sandbox_api_key.txt") |> String.trim,
+      gdax_api_secret: File.read!("config/secrets/gdax_sandbox_api_secret.txt") |> String.trim,
+      gdax_api_password: File.read!("config/secrets/gdax_sandbox_api_password.txt") |> String.trim
+end
